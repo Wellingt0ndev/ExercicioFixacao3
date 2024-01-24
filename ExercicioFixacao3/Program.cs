f1f87c8d4c74167038e2a8f89943021262c992f7
Wellington Ferreira da Silva <108769429+Wellingt0ndev@users.noreply.github.com>
2024-01-24 16:53:19 -0300

    Solved exercise

---
+++ ExercicioFixacao3/Program.cs
@@ -0,0 +1,45 @@
+﻿using ExercicioFixacao3.Entities;
+using System.Globalization;
+
+List<Product> products = new List<Product>();
+
+Console.Write("Enter the number of products: ");
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 1;  i <= n; i++)
+{
+    Console.WriteLine($"Product #{i} data: ");
+    Console.Write("Common, used or imported (c/u/i)? ");
+    char ch = char.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Price: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    if(ch == 'u')
+    {
+        Console.Write("Manufacture date (DD/MM/YYYY): ");
+        DateTime date = DateTime.Parse(Console.ReadLine());
+        UsedProduct used = new UsedProduct(name,price,date);
+        products.Add(used);
+    } else if(ch == 'i')
+    {
+        Console.Write("Customs fee: ");
+        double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        ImportedProduct imported = new ImportedProduct(name,price,customsFee);
+        products.Add(imported);
+    }
+    else
+    {
+        Product product = new Product(name, price);
+        products.Add(product);
+    }
+
+    
+}
+
+Console.WriteLine();
+Console.WriteLine("Price Tags:");
+foreach (Product product in products)
+{
+    Console.WriteLine(product.PriceTag());
+}
